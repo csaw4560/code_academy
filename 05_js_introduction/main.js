a393e5d894339532203540eaabaeca030a9f78c5
David Reisenauer <David@Davids-MacBook-Pro-3.local>
2020-03-22 19:51:26 +0100

    String Concatenation

--- 05_js_introduction/main.js
+++ 05_js_introduction/main.js
@@ -1,5 +1,2 @@
-console.log(42 + 3.5);
-//tja dauert wohl noch //
-console.log(2020 - 1969);
-console.log(65 / 240);
-console.log(0.2708 * 100);
+console.log('Hello' + 'World')
+console.log('Hello' + ' ' + 'World')
