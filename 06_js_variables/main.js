f9543600ef4eacfa41f39c6a87fe63a71419ca96
David Reisenauer <David@Davids-MacBook-Pro-3.local>
2020-03-23 18:34:13 +0100

    String Interpolation

--- 06_js_variables/main.js
+++ 06_js_variables/main.js
@@ -1,2 +1,3 @@
-let favoriteAnimal = 'Lama'
-console.log('My favorite animal: ' + favoriteAnimal)
+const myName = 'James Bond'
+const myCity = 'London'
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
