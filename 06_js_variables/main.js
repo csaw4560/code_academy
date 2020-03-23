ed2e11df489c2fed82f74e7847b555e12433db98
David Reisenauer <David@Davids-MacBook-Pro-3.local>
2020-03-23 18:37:18 +0100

    typeof operator

--- 06_js_variables/main.js
+++ 06_js_variables/main.js
@@ -1,3 +1,4 @@
-const myName = 'James Bond'
-const myCity = 'London'
-console.log(`My name is ${myName}. My favorite city is ${myCity}`);
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
